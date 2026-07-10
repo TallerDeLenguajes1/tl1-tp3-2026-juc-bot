f50677d0817922971c25c01994733b376ea14fef
Julian <jc6070060@gmail.com>
2026-07-10 16:19:04 -0300

    Resolver conflicto

--- ArregloDeNombres.c
+++ ArregloDeNombres.c
@@ -4,6 +4,7 @@
 #define TAMA 5
 
 void MostrarPersonas(char **nombre, int tama);
+int BuscarNombre(char **nombre, char *palabra, int tama);
 
 int main()
 {
@@ -17,8 +18,13 @@ int main()
         nombre[i] = (char *)malloc((strlen(buff)+1)*sizeof(char));
         strcpy(nombre[i], buff);
     }
-    
     MostrarPersonas(nombre, TAMA);
+
+    char *palabra = malloc(100 * sizeof(char));
+    printf("Ingrese la palabra\n");
+    gets(palabra);
+    BuscarNombre(nombre, palabra, TAMA);
+
     return 0;
 }
 
@@ -29,4 +35,16 @@ void MostrarPersonas(char **nombre, int tama)
         printf("---NOMBRE N %d---\n", i+1);
         puts(nombre[i]);
     }
+}
+
+int BuscarNombre(char **nombre, char *palabra, int tama)
+{
+    for (int i = 0; i < tama; i++)
+    {
+        if (strstr(nombre[i], palabra))
+        {
+            return i;
+        }
+    }
+    return -1;
 }
